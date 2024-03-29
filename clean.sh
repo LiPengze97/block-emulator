90b5623e7a67db2ebf57a671e0e73cea84301140
lipengze97 <vonei@126.com>
2024-03-29 07:47:36 -0600

    cmp

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf ./record/*
+rm -rf ./result/*
+rm -rf ./log/*
--- params/global_config.go
+++ params/global_config.go
@@ -3,14 +3,15 @@ package params
 var (
 	Block_Interval      = 5000   // generate new block interval
 	MaxBlockSize_global = 2000   // the block contains the maximum number of transactions
-	InjectSpeed         = 2000   // the transaction inject speed
-	TotalDataSize       = 100000 // the total number of txs
-	BatchSize           = 16000  // supervisor read a batch of txs then send them, it should be larger than inject speed
+	InjectSpeed         = 18000   // the transaction inject speed 22250 for 32
+	TotalDataSize       = 1000000 // the total number of txs
+	BatchSize           = 36000  // supervisor read a batch of txs then send them, it should be larger than inject speed
 	BrokerNum           = 10
-	NodesInShard        = 4
-	ShardNum            = 4
-	DataWrite_path      = "./result/"                                // measurement data result output path
-	LogWrite_path       = "./log"                                    // log output path
-	SupervisorAddr      = "127.0.0.1:18800"                          //supervisor ip address
-	FileInput           = `../2000000to2999999_BlockTransaction.csv` //the raw BlockTransaction data path
+	NodesInShard        = 3
+	ShardNum            = 32
+	DataWrite_path      = "./result/"       // measurement data result output path
+	LogWrite_path       = "./log"           // log output path
+	SupervisorAddr      = "127.0.0.1:18800" //supervisor ip address
+	// FileInput           = `/mnt/e/eth_data/11000000to11999999_BlockTransaction.csv`
+	FileInput = `/users/pzl97/17000000to17249999_BlockTransaction_first_5_million.csv`
 )
